423f02701e55a27a785d45b253210ea31f9c33f3
thakyZ <thakyz@outlook.com>
2022-08-22 08:47:16 -0700

    Updated to use Powercord command api

--- index.js
+++ index.js
@@ -2,37 +2,31 @@ const { Plugin } = require("powercord/entities");
 const { inject, uninject } = require("powercord/injector");
 const { messages } = require("powercord/webpack");
 
-let prefix;
-module.exports = class timestamp extends Plugin {
+module.exports = class ReplaceSteamLinks extends Plugin {
   startPlugin() {
-    prefix = "\\.steam=";
-    this.patchMessage();
-  }
-  patchMessage() {
-    //Lighty made this better because he felt like it
-    inject(
-      "steam-link",
-      messages,
-      "sendMessage",
-      (args) => {
-        const regexAGlobal = new RegExp(
-          `${prefix}(https:\/\/(store\.)?steam(powered|community)\.com\/.*)`,
-          "gi"
-        );
-        if (args[1].content.search(regexAGlobal) !== -1) {
-          args[1].content = args[1].content.replace(
-            regexAGlobal,
-            "steam://openurl/$1"
-          );
+    powercord.api.commands.registerCommand({
+      command: "steam",
+      description:
+        "Replaces Steam links to open in the app for other users without Powercord.",
+      usage: "{c} <url>",
+      executor: (args) => {
+        const regexAGlobal =
+          /(https:\/\/(store\.)?steam(powered|community)\.com\/.*)/gi;
+        if (args[0].search(regexAGlobal) !== -1) {
+          return {
+            send: true,
+            result: `${args[0].replace(regexAGlobal, "steam://openurl/$1")}`,
+          };
+        } else {
+          return {
+            send: false,
+            result: `Invalid Steam link: ${args[0]}`,
+          };
         }
-        return args;
       },
-      true
-    );
+    });
   }
   pluginWillUnload() {
-    uninject("steam-link");
-    powercord.api.settings.unregisterSettings(this.entityID);
-    powercord.api.settings.unregisterSettings(this.entityID);
+    powercord.api.commands.unregisterCommand("steam");
   }
 };
